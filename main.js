c66da22889b0ad82b76dc3e2a95668d977bf2e02
Maria Khmel <mariakhmel8@gmail.com>
2024-11-01 22:48:58 +0200

    setup

---
+++ main.js
@@ -0,0 +1,16 @@
+// Користувач вводить вік.Напишіть функцію,
+//   яка перевіряє, чи є введене число
+// віком, і повертає true,
+//   якщо це правда, і false – якщо ні.
+//  Критерії віку:
+// - ціле число;
+// - Більше 0
+
+const checkUserAge = () => {
+  const userAgeInput = prompt('What is your age?');
+  const result = userAgeInput;
+  console.log(result)
+}
+
+console.log(checkUserAge())
+
