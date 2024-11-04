9e58142587a0f636210ed3606229f03ffe7a60c4
Maria Khmel <mariakhmel8@gmail.com>
2024-11-04 02:18:26 +0200

    class

--- main.js
+++ main.js
@@ -356,3 +356,48 @@ const floors = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // storage.removeItem('🍋');
 // console.log(storage.items); // [ '🍎', '🍇', '🍑', '🍌' ]
+
+
+// class User {
+//   #login;
+//   #email;
+
+//   constructor({ login, email }) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+
+//   get login() {
+//     return this.#login;
+//   }
+
+//   set login(newLogin) {
+//     this.#login = newLogin;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail
+//   }
+// }
+
+// const mango = new User({
+//   login: 'Mango',
+//   email: 'mango@dog.woof',
+// });
+
+// console.log(mango.login); // Mango
+// mango.login = 'Mangodoge';
+// console.log(mango.login); // Mangodoge
+
+// const poly = new User({
+//   login: 'Poly',
+//   email: 'poly@mail.com',
+// });
+
+// console.log(poly.login); // Poly
+// poly.login = 'Polycutie';
+// console.log(poly.login); // Polycutie
