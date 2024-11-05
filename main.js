7cce7d9561e8196578f9fafffb93ee878e771793
Maria Khmel <mariakhmel8@gmail.com>
2024-11-05 23:39:55 +0200

    map

--- main.js
+++ main.js
@@ -554,3 +554,27 @@ const floors = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // calculator.read(1, 2);
 // console.log(calculator.add())
+
+const cars = [
+  { make: 'Honda', model: 'CR-V', type: 'suv', amount: 14, price: 24045, onSale: true },
+  { make: 'Honda', model: 'Accord', type: 'sedan', amount: 2, price: 22455, onSale: true },
+  { make: 'Mazda', model: 'Mazda 6', type: 'sedan', amount: 8, price: 24195, onSale: false },
+  { make: 'Mazda', model: 'CX-9', type: 'suv', amount: 7, price: 31520, onSale: true },
+  { make: 'Toyota', model: '4Runner', type: 'suv', amount: 19, price: 34210, onSale: false },
+  { make: 'Toyota', model: 'Sequoia', type: 'suv', amount: 16, price: 45560, onSale: false },
+  { make: 'Toyota', model: 'Tacoma', type: 'truck', amount: 4, price: 24320, onSale: true },
+  { make: 'Ford', model: 'F-150', type: 'truck', amount: 11, price: 27110, onSale: true },
+  { make: 'Ford', model: 'Fusion', type: 'sedan', amount: 13, price: 22120, onSale: true },
+  { make: 'Ford', model: 'Explorer', type: 'suv', amount: 6, price: 31660, onSale: false }
+];
+
+// const getModels = cars => cars.map(car => car.model);
+
+// console.table(getModels(cars));
+
+// const makeCarsWithDiscount = (cars, discount) => cars.map(car => {
+//   return { ...car, price: car.price / (1 - discount) }
+// });
+
+// console.table(makeCarsWithDiscount(cars, 0.2));
+// console.table(makeCarsWithDiscount(cars, 0.4));
