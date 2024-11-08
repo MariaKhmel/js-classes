d5bccf91f0031f649499c078be3f8e04b6632b76
Maria Khmel <mariakhmel8@gmail.com>
2024-11-08 23:56:23 +0200

    destructure

--- main.js
+++ main.js
@@ -848,3 +848,33 @@ function printContactsInfo({ names, phones }) {
 //     },
 //   }),
 // ); // "Cyberdyne Systems has 200 bots in stock"
+
+// Solution
+function getStockReport({ companyName, stock }) {
+  let total = 0;
+  for (const value of Object.values(stock)) {
+    total += value;
+  }
+  return `${companyName} has ${total} items in stock`;
+}
+
+console.log(
+  getStockReport({
+    companyName: 'Cyberdyne Systems',
+    stock: {
+      repairBots: 150,
+      defenceBots: 50,
+    },
+  }),
+); // "Cyberdyne Systems has 200 items in stock"
+
+console.log(
+  getStockReport({
+    companyName: 'Belacci',
+    stock: {
+      shoes: 20,
+      skirts: 10,
+      hats: 5,
+    },
+  }),
+); // "Belacci has 35 item in stock"
