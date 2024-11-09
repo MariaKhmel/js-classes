b727b4e9ea87f882ca3dfa2d7367294aaa7626ca
Maria Khmel <mariakhmel8@gmail.com>
2024-11-10 01:37:20 +0200

    account

--- main.js
+++ main.js
@@ -893,3 +893,100 @@ function printContactsInfo({ names, phones }) {
 // }
 
 // console.log(calcTotalPrice(stones, 'Rubble'))
+
+
+
+/*
+ * There are only two types of transaction.
+ * You can deposit or withdraw money from your account.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Each transaction is an object with properties: id, type and amount
+ */
+
+// const account = {
+//   // Current account balance
+//   balance: 0,
+
+//   // Transaction History
+//   transactions: [],
+
+//   /*
+//    * Method creates and returns a transaction object.
+//    * Accepts amount and type of transaction.
+//    */
+//   createTransaction(amount, type) {
+//     return {
+//       id: 1,
+//       type: type,
+//       amount: amount,
+//     }
+
+//   },
+
+//   /*
+//    * The method responsible for adding the amount to the balance..
+//    * Accepts the amount of the transaction.
+//    * Calls createTransaction to create a transaction object
+//    * then adds it to the transaction history
+//    */
+//   deposit(amount) {
+//     const newTransaction = this.createTransaction(amount, Transaction.DEPOSIT);
+//     this.transactions.push(newTransaction);
+//     this.balance += amount;
+//   },
+
+//   /*
+//    *The method responsible for withdrawing the amount from the balance.
+//    * Принимает сумму танзакции.
+//    * Calls createTransaction to create a transaction object
+//    * then adds it to the transaction history.
+//    *
+//    * If amount is greater than the current balance, display a message
+//    * about the fact that the withdrawal of such an amount is not possible, there are not enough funds.
+//    */
+//   withdraw(amount) {
+//     if (amount > this.balance) {
+//       return 'Not enough funds'
+//     }
+//     const newTransaction = this.createTransaction(amount, Transaction.WITHDRAW);
+//     this.transactions.push(newTransaction);
+//     this.balance -= amount;
+//   },
+
+//   /*
+//    * The method returns the current balance
+//    */
+//   getBalance() {
+//     return this.balance;
+//   },
+
+//   /*
+//    * The method searches and returns the transaction object by id
+//    */
+//   getTransactionDetails(id) {
+//     return this.transactions.find(transaction => transaction.id === id);
+//   },
+
+//   /*
+//    * The method returns the amount of funds
+//    *a specific type of transaction from the entire history of transactions
+//    */
+//   getTransactionTotal(type) {
+//     return this.transactions.filter(transaction => transaction.type === type)
+//   },
+// };
+
+
+// account.deposit(1000);
+// console.log(account.transactions)
+// account.withdraw(300);
+// console.log(account.transactions)
+// console.log(account.balance)
+// console.log(account.getTransactionDetails(1))
+// console.log(account.getTransactionTotal(Transaction.DEPOSIT))
